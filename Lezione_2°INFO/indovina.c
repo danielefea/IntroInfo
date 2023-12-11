7aad5b5cdfbc870c64fd0d4a188353217e490a56
Daniele Fea <daniele.fea@vallauri.edu>
2023-12-11 14:41:22 +0100

    Aggiunti dei commenti e tolte alcune parti da completare con la classe

--- Lezione_2°INFO/indovina.c
+++ Lezione_2°INFO/indovina.c
@@ -3,18 +3,14 @@
 #include <stdlib.h>
 
 int main(){
+    //GESTIONE DELLA MEMORIA
     int tentativo=0, nNascosto=0, nTentativi = 0;
+
+    //GENERAZIONE DEL NUMERO NASCOSTO
     srand(time(0));
     nNascosto = rand()%5+1;
 
-    do{
-        printf("Prova ad indovinare il numero nascosto: ");
-        scanf("%d", &tentativo);
-        nTentativi++;
-    }while(nNascosto != tentativo && nTentativi<4);
-    if(nNascosto == tentativo){
-        printf("Ottimo, hai indovinato il numero!");
-    }else{
-        printf("Hai finito i tentativi e quindi hai perso! Il numero era %d", nNascosto);
-    }
+    //RICHIEDO ALL'UTENTE DI FARE UN TENTATIVO
+    printf("Prova ad indovinare il numero nascosto: ");
+    scanf("%d", &tentativo);
 }
